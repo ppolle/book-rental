8a3f5415d2c098159f85adabcdd6a9b08e257a2a
ppolle <peter.m.polle@gmail.com>
2020-11-28 02:52:21 +0300

    this commit will update initial index page code

--- booker/apps/rentalapp/views.py
+++ booker/apps/rentalapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+	'''
+	Render the index page
+	'''
+	return render(request, 'rentalapp/index.html')
