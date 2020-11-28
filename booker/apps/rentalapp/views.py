5bd6c73ae538304e849e768ffc259b166e527ca8
ppolle <peter.m.polle@gmail.com>
2020-11-28 12:03:34 +0300

    this commit will update index page UI

--- booker/apps/rentalapp/views.py
+++ booker/apps/rentalapp/views.py
@@ -1,3 +1,5 @@
+from booker.apps.libraryapp.models import Book
+from booker.apps.rentalapp.cart import Cart
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -5,4 +7,19 @@ def index(request):
 	'''
 	Render the index page
 	'''
-	return render(request, 'rentalapp/index.html')
+	books =  Book.objects.all()
+	return render(request, 'rentalapp/index.html', {"books":books})
+
+def book_details(request, book_id):
+	'''
+	view book details
+	'''
+	book = Book.objects.get(id=book_id)
+	return render(request, 'rentalapp/book_details.html', {"book":book})
+
+def rent_book(request, book_id):
+	'''
+	Add book details to rent data
+	'''
+	cart = Cart()
+	pass
