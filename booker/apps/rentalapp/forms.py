83751ac1ae3e4719cbad64009264b30bd7c64919
ppolle <peter.m.polle@gmail.com>
2020-11-28 23:20:07 +0300

    this commit will update custom validation messages

--- booker/apps/rentalapp/forms.py
+++ booker/apps/rentalapp/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 class RentBookForm(forms.Form):
 	start_date = forms.DateField()
 	end_date = forms.DateField()
+
+	def clean_start_date(self):
+		from datetime import date
+		now = date.today()
+		if self.cleaned_data['start_date'] < now:
+			raise forms.ValidationError('The start date cannot be in the past')
+
+		return self.cleaned_data['start_date']
+
+	def clean_end_date(self):
+		if self.cleaned_data['end_date'] <= self.cleaned_data['start_date']:
+			raise forms.ValidationError('End date can only come after start date')
+
+		return self.cleaned_data['end_date']
