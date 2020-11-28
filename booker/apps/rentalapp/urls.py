358599f9af2973bc2372f391f16c36344cfa2edd
ppolle <peter.m.polle@gmail.com>
2020-11-28 14:39:01 +0300

    this commit will update new book details functionality

--- booker/apps/rentalapp/urls.py
+++ booker/apps/rentalapp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from booker.apps.rentalapp import views
 urlpatterns = [
     path('', views.index,name='index'),
+    path('book/<int:book_id>/', views.book_details, name='book_details'),
    
 ]
