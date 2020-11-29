1e6694d35b0ca8fac34ca407b953b738f53f604f
ppolle <peter.m.polle@gmail.com>
2020-11-29 14:43:18 +0300

    this commit will update form validations

--- booker/apps/rentalapp/forms.py
+++ booker/apps/rentalapp/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from datetime import date
 
 class DateInput(forms.DateInput):
 	input_type = 'date'
+
 class RentBookForm(forms.Form):
 	start_date = forms.DateField(widget=DateInput)
 	end_date = forms.DateField(widget=DateInput)
 
 	def clean_start_date(self):
-		from datetime import date
 		now = date.today()
 		if self.cleaned_data['start_date'] < now:
 			raise forms.ValidationError('The start date cannot be in the past')
@@ -16,6 +17,9 @@ class RentBookForm(forms.Form):
 		return self.cleaned_data['start_date']
 
 	def clean_end_date(self):
+		if self.cleaned_data['end_date'] < date.today():
+			raise forms.ValidationError('This date cannot be in the past')
+
 		if self.cleaned_data['start_date'] > self.cleaned_data['end_date']:
 			raise forms.ValidationError('End date can only come after start date')
 
