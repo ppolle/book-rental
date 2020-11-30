2d0c3edffa8d521dbb69e89d0ca8969c214175d1
ppolle <peter.m.polle@gmail.com>
2020-11-30 11:16:52 +0300

    this commit will update normal library app tests

--- booker/apps/libraryapp/tests/test_models.py
+++ booker/apps/libraryapp/tests/test_models.py
@@ -25,3 +25,35 @@ class LibraryAppTests(TestCase):
 
 		cost = book.calculate_cost(start_date, stop_date)
 		self.assertEqual(cost,4.5)
+
+	def test_calculate_cost_with_minimal_days(self):
+		novel = BookType.objects.create(name='novel', daily_rate=1.0,minimum_days=3)
+		book = Book(title='a man of the people', genre=novel, description="A man's people")
+		start_date = date.today()
+		stop_date = date.today()+timedelta(days=6)
+		cost = book.calculate_cost(start_date, stop_date)
+		self.assertEqual(cost,6)
+
+	def test_calculate_cost_with_total_days_more_than_minimal_days(self):
+		novel = BookType.objects.create(name='novel', daily_rate=1.0,minimum_days=3)
+		book = Book(title='a man of the people', genre=novel, description="A man's people")
+		start_date = date.today()
+		stop_date = date.today()+timedelta(days=5)
+		cost = book.calculate_cost(start_date, stop_date)
+		self.assertEqual(cost,5)
+
+	def test_calculate_cost_with_minimal_days_and_early_rates(self):
+		novel = BookType.objects.create(name='novel', daily_rate=2.0,minimum_days=3, early_rates=1)
+		book = Book(title='a man of the people', genre=novel, description="A man's people")
+		start_date = date.today()
+		stop_date = date.today()+timedelta(days=1)
+		cost = book.calculate_cost(start_date, stop_date)
+		self.assertEqual(cost,3)
+
+	def test_calculate_cost_with_minimal_days_and_early_rates_with_more_days(self):
+		novel = BookType.objects.create(name='novel', daily_rate=2.0,minimum_days=3, early_rates=1)
+		book = Book(title='a man of the people', genre=novel, description="A man's people")
+		start_date = date.today()
+		stop_date = date.today()+timedelta(days=5)
+		cost = book.calculate_cost(start_date, stop_date)
+		self.assertEqual(cost,7)
